688c7192306c46addb948dbdf79d25db46fcebab
Arpit Jana <arpitjana2103@gmail.com>
2026-01-29 19:44:27 +0530

    [be] refactor ( server ) : refactor server

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,22 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
-const connectDB = require("./configs/db.config");
+const connectDB = require("./configs/mongodb.config");
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, connectDB);
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on port ${PORT}`);
+            console.log(`🔗 api url : http://127.0.0.1:${PORT}`);
+        });
+    } catch (error) {
+        console.error("❌ Failed to start server:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
